28be06face8f9a58e6fb5261076f137d43d013f2
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-01-12 22:08:58 +0700

    chore: add vue motion

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,6 +1,20 @@
 <template>
   <VoteCard />
   <TheTable />
+  <h1
+    v-motion
+    class="text-3xl"
+    :initial="{
+      opacity: 0,
+      y: 100,
+    }"
+    :enter="{
+      opacity: 1,
+      y: 0,
+    }"
+  >
+    Hello 👋
+  </h1>
 </template>
 
 <route lang="yaml">
