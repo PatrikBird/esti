a99f95a55e6c610df51a71ea177d2fa0488e59de
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-01-15 16:35:26 +0700

    chore: add types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,19 @@
 import { type ViteSSGContext } from 'vite-ssg'
 
 export type UserModule = (ctx: ViteSSGContext) => void
+
+export interface User {
+  id: number
+  name: string
+  voteValue: string
+  isObserver: boolean
+  lastVote: Date
+}
+
+export interface SessionState {
+  id: number
+  voting: boolean
+  isRevealed: boolean
+  lastVoteReaveal: Date
+  lastVoteReset: Date
+}
