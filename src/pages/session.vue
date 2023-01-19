983fe894968279dfc83ee3576223f75cacb7ac56
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-01-19 10:09:38 +0700

    debug: try to set data bindings to dbs

--- src/pages/session.vue
+++ src/pages/session.vue
@@ -1,4 +1,29 @@
+<script setup lang='ts'>
+import { useCollection, useDatabaseList, useDatabaseObject, useDocument } from 'vuefire'
+import { ref as dbRef, getDatabase } from 'firebase/database'
+import { collection, doc, getFirestore } from 'firebase/firestore'
+import { FSdb } from '../modules/firebase'
+
+const coll = useCollection(collection(FSdb, 'someID'))
+const docc = useDocument(doc(collection(FSdb, 'someID'), 'voteState'))
+console.log(coll)
+console.log(docc)
+
+// const route = '1234-session-ID-56789'
+// const db = getDatabase()
+// const todos = useDatabaseList(dbRef(db, route))
+// const someTodo = useDatabaseObject(dbRef(db, route))
+
+// console.log(todos)
+// console.log(someTodo)
+</script>
+
 <template>
+  <div>
+    <p>something here?</p>
+    <p>whole collection: {{ coll }}</p>
+    <p>just a single doc: {{ docc }}</p>
+  </div>
   <VoteCards
     :available-votes="[
       '0',
