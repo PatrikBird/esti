b583c078ba8310b2eb15b6d2a1e9307786191d01
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-02-05 14:30:46 +0700

    fix: styling of vote result progress bars
    todo: order is not rendered correct

--- src/components/ProgressBar.vue
+++ src/components/ProgressBar.vue
@@ -1,33 +1,21 @@
 <script setup lang='ts'>
+import type { ComputedRef } from 'vue'
+
 const props = defineProps<{ name: string; voteValue: string | null; maxVote?: number }>()
 
-const val = computed(() =>
-  (props.voteValue === '?' || props.voteValue === 'break') ? '100' : props.voteValue,
-)
+// const val = computed(() =>
+//   (props.voteValue === '?' || props.voteValue === 'break') ? '100' : props.voteValue,
+// )
+const val: ComputedRef<string> = computed(() => {
+  let percentage = 100
+  if (props.voteValue && props.voteValue !== '?' && props.voteValue !== 'break')
+    percentage = (parseInt(props.voteValue) / props.maxVote!) * 100
 
-const progressWidth = computed(() => {
-  if (val.value === null)
-    return 'w-0'
-  if (val.value === 'coffee' || val.value === '?')
-    return 'w-100'
-
-  // if (props.maxVote)
-  //   return `w-${Math.round((+val.value / props.maxVote) * 100)}%`
-
-  return `w-[${val.value}%]`
-})
-
-watchEffect(() => {
-  console.log('progressWidth', progressWidth.value)
+  return `${percentage}`
 })
 </script>
 
 <template>
-  <!-- <div class="w-full rounded-full bg-gray-200 dark:bg-gray-700">
-    <div class="rounded-full bg-blue-600 p-0.5 text-center text-xs font-medium leading-none text-blue-100" style="width: 45%">
-      45%
-    </div>
-  </div> -->
   <dl>
     <dt class="pt-2 text-sm font-medium text-zinc-500 dark:text-zinc-400 sm:pt-0">
       {{ name }}
@@ -35,8 +23,7 @@ watchEffect(() => {
     <dd class="mb-3 flex items-center">
       <div class="mr-2 h-2.5 w-full rounded bg-zinc-200 dark:bg-zinc-700">
         <div
-          :class="progressWidth"
-          class="h-2.5 rounded bg-indigo-600 dark:bg-indigo-500"
+          class="fill-me h-2.5 rounded bg-indigo-600 dark:bg-indigo-500"
         />
       </div>
       <div class="float-right w-1/12">
@@ -48,3 +35,9 @@ watchEffect(() => {
     </dd>
   </dl>
 </template>
+
+<style scoped>
+.fill-me {
+width: v-bind('`${val}%`');
+}
+</style>
--- src/components/VoteResults.vue
+++ src/components/VoteResults.vue
@@ -2,16 +2,46 @@
 import type { User } from '../types'
 
 const props = defineProps<{ voters?: User[] }>()
+
 const votersWithoutNull = computed(() => props.voters?.filter(v => v.voteValue !== null))
+
+const sortedVotersWithoutNull = computed(() => {
+  if (!votersWithoutNull.value)
+    return undefined
+  const voters = votersWithoutNull.value.slice()
+  voters.sort((a, b) => {
+    if (a.voteValue === '?')
+      return 1
+    if (b.voteValue === '?')
+      return -1
+    if (a.voteValue === 'coffee')
+      return 1
+    if (b.voteValue === 'coffee')
+      return -1
+    return +a.voteValue! - +b.voteValue!
+  })
+
+  return voters
+})
+const votersThatVotedANumber = computed(() =>
+  votersWithoutNull.value?.filter(u => u.voteValue !== null && u.voteValue !== '?' && u.voteValue !== 'coffee'),
+)
+const maxVote = computed(() =>
+  votersThatVotedANumber.value?.reduce((max, v) => Math.max(max, +v.voteValue!), 0),
+)
 </script>
 
 <template>
-  <average-vote :voters="votersWithoutNull" />
-  <div class="sm:grid sm:grid-cols-2 sm:gap-y-2 sm:gap-x-8">
+  <average-vote
+    :voters="sortedVotersWithoutNull"
+    class="pl-2 sm:pl-0"
+  />
+  <div class="grid gap-y-2 gap-x-8 sm:grid-cols-2">
     <progress-bar
       v-for="{ id, name, voteValue } in votersWithoutNull" :key="id"
       :name="name"
       :vote-value="voteValue"
+      :max-vote="maxVote"
     />
   </div>
 </template>
