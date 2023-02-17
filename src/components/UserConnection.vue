347df02ed34d4fd282c19af70150be78c63669ac
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-02-17 12:42:12 +0900

    chore: prevent closing modal when click on backdrop

--- src/components/UserConnection.vue
+++ src/components/UserConnection.vue
@@ -23,7 +23,7 @@ function claimExistingUser() {
 
 <template>
   <TransitionRoot as="template" :show="open">
-    <Dialog as="div" class="relative z-10" @close="open = false">
+    <Dialog as="div" class="relative z-10">
       <TransitionChild as="template" enter="ease-out duration-300" enter-from="opacity-0" enter-to="opacity-100" leave="ease-in duration-200" leave-from="opacity-100" leave-to="opacity-0">
         <div class="fixed inset-0 bg-zinc-500/75 transition-opacity" />
       </TransitionChild>
