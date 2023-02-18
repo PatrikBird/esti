292211fa719121bdaf5c7e678cfa94fe095f8bfa
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-02-18 16:26:12 +0900

    chore: add draft of about page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,8 +1,34 @@
 <template>
-  <main class="max-w-2xl p-10">
-    <h1 class="text-3xl font-semibold">
-      Hello :)
-    </h1>
-    <p>My name is Patrik</p>
+  <main class="p-20">
+    <div class="mx-auto max-w-xl text-left">
+      <h1 class="text-3xl font-semibold leading-loose">
+        Hello :)
+      </h1>
+      <p>
+        I am <a
+          href="https://patbird.me"
+          target="_blank"
+          class="link-underline"
+        >Patrik</a>
+        and `esti` is a fun side project of mine.
+        If you find any bugs, please let me know by creating
+        an
+        <a
+          href="https://github.com/PatrikBird/esti/issues/new"
+          target="_blank"
+          class="link-underline"
+        >issue on GitHub</a>.
+      </p>
+      <br>
+      <p>
+        If you use and like esti, consider
+        <a
+          href="https://www.buymeacoffee.com/patbird"
+          target="_blank"
+          class="link-underline"
+        >buying me a coffee</a>
+        to help maintaining this project.
+      </p>
+    </div>
   </main>
 </template>
