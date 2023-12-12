818eeef7092f6a70454e2c4d8790dd035f3b98c2
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-12-12 12:59:15 +0700

    chore: cleanup unocss cfg

--- unocss.config.ts
+++ unocss.config.ts
@@ -13,9 +13,7 @@ import transformerVariantGroup from '@unocss/transformer-variant-group'
 export default defineConfig({
   shortcuts: [
     {
-      // 'bg-base': 'bg-white dark:bg-black',
-      'border-base': 'border-[#8884]',
-      'myLink': 'font-semibold text-blue-600 no-underline hover:underline dark:text-blue-400',
+      myLink: 'font-semibold text-blue-600 no-underline hover:underline dark:text-blue-400',
     },
     // [/^btn-(\w+)$/, ([_, color]) => `op50 px2.5 py1 transition-all duration-200 ease-out no-underline! hover:(op100 text-${color} bg-${color}/10) border border-base! rounded`],
   ],
@@ -41,8 +39,6 @@ export default defineConfig({
       provider: 'bunny',
       fonts: {
         sans: 'Inter:400,600,800',
-        mono: 'DM Mono:400,600',
-        mont: 'Montserrat:600,700,800',
       },
     }),
   ],
