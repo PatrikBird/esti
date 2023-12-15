a03b63a7fe717eb2e196185ed921b9713961df60
Patrik Bird <52401692+PatrikBird@users.noreply.github.com>
2023-12-15 17:40:52 +0700

    Feat/copy url btn (#21)
    feat: copy URL to clipboard button
    
    uses floating UI for tooltip

---
+++ src/modules/floatingVue.ts
@@ -0,0 +1,7 @@
+import FloatingVue from 'floating-vue'
+import type { UserModule } from '~/types'
+import 'floating-vue/dist/style.css'
+
+export const install: UserModule = ({ app }) => {
+  app.use(FloatingVue)
+}
