81f747b510f32a927d8067dbbb4f759efc42232f
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-12-15 17:04:34 +0700

    feat: copy URL to clipboard button
    uses floating UI for tooltip

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,34 +1,25 @@
 <script setup lang='ts'>
 const mainStore = useMainStore()
+const route = useRoute()
 
 const lastSessionLink = computed(() =>
   mainStore.session.id === '' ? false : `/session/${mainStore.session.id}`,
 )
+
+const source = ref(`${window.location.origin}${route.path}`)
+const { copy, copied } = useClipboard({ source })
+const routeContainsSession = computed(() => route.path.includes('session'))
 </script>
 
 <template>
   <nav class="top-0 z-10 h-16 pt-6">
-    <div
-      class="w-full sm:px-8"
-    >
+    <div class="w-full sm:px-8">
       <div class="mx-auto max-w-7xl lg:px-8">
         <div class="relative px-4 sm:px-8">
           <div class="mx-auto max-w-2xl">
             <div class="relative flex gap-4">
               <div class="flex flex-1 justify-center gap-3 md:justify-center">
-                <!-- <div class="pointer-events-auto">
-                  <button
-                    type="button"
-                    aria-label="Toggle dark mode"
-                    title="Toggle dark mode"
-                    class="group rounded-full px-3 py-2 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 hover:text-blue-500 dark:text-blue-500 dark:ring-white/10 dark:hover:text-blue-400 dark:hover:ring-white/20"
-                    @click="toggleDark()"
-                  >
-                    <span i-line-md:sunny-filled-loop v-if="!isDark" class="h-[1.3rem] w-[1.3rem] " />
-                    <span i-line-md:sunny-filled-loop-to-moon-filled-loop-transition v-else class="h-[1.35rem] w-[1.35rem]" />
-                  </button>
-                </div> -->
-                <nav class="pointer-events-auto block">
+d                <nav class="pointer-events-auto block">
                   <ul class="list-none flex rounded-full bg-white px-3 text-sm shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:bg-zinc-800 dark:text-zinc-200 dark:ring-white/10">
                     <li>
                       <RouterLink
@@ -60,19 +51,42 @@ const lastSessionLink = computed(() =>
                     </li>
                   </ul>
                 </nav>
-                <div class="pointer-events-auto">
+                <button
+                  type="button"
+                  aria-label="Toggle dark mode"
+                  title="Toggle dark mode"
+                  class="group rounded-full px-3 py-2 bg-white shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 hover:text-blue-500
+                dark:(bg-zinc-800 text-blue-500 ring-white/10 hover:text-blue-400 hover:ring-white/20)"
+                  @click="toggleDark()"
+                >
+                  <span v-if="!isDark" i-line-md:sunny-filled-loop class="h-[1.3rem] w-[1.3rem]" />
+                  <span v-else i-line-md:sunny-filled-loop-to-moon-filled-loop-transition class="h-[1.35rem] w-[1.35rem]" />
+                </button>
+                <VDropdown
+                  v-if="routeContainsSession"
+                  :distance="7"
+                  :triggers="[]"
+                  :shown="copied"
+                  :auto-hide="false"
+                  title="Copy URL to clipboard"
+                  aria-label="Copy URL to clipboard"
+                >
                   <button
                     type="button"
-                    aria-label="Toggle dark mode"
-                    title="Toggle dark mode"
                     class="group rounded-full px-3 py-2 bg-white shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 hover:text-blue-500
-                    dark:(bg-zinc-800 text-blue-500 ring-white/10 hover:text-blue-400 hover:ring-white/20)"
-                    @click="toggleDark()"
+                  dark:(bg-zinc-800 text-white ring-white/10 hover:text-blue-400 hover:ring-white/20)"
+                    :class="copied ? 'text-blue-500 dark:text-blue-500!' : 'text-zinc-900 dark:text-zinc-200'"
+                    @click="copy()"
                   >
-                    <span v-if="!isDark" i-line-md:sunny-filled-loop class="h-[1.3rem] w-[1.3rem] " />
-                    <span v-else i-line-md:sunny-filled-loop-to-moon-filled-loop-transition class="h-[1.35rem] w-[1.35rem]" />
+                    <span v-if="!copied" i-fluent:clipboard-24-regular class="h-[1.3rem] w-[1.3rem]" />
+                    <span v-else i-fluent:clipboard-checkmark-24-regular class="h-[1.3rem] w-[1.3rem]" />
                   </button>
-                </div>
+                  <template #popper>
+                    <p class="p2 text-xs bg-zinc-800 text-white dark:(text-zinc-800 bg-zinc-100)">
+                      URL Copied!
+                    </p>
+                  </template>
+                </VDropdown>
               </div>
             </div>
           </div>
@@ -81,3 +95,11 @@ const lastSessionLink = computed(() =>
     </div>
   </nav>
 </template>
+
+<style lang="postcss">
+.v-popper__wrapper 
+.v-popper__arrow-container 
+.v-popper__arrow-inner {
+  @apply border-zinc-800 dark:border-zinc-100;
+}
+</style>
