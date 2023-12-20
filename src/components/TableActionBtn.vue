c1c04915f19ed2b15d9ec153d52c0c42e71324a3
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2023-12-20 16:34:01 +0700

    fix: border styles on table action button

--- src/components/TableActionBtn.vue
+++ src/components/TableActionBtn.vue
@@ -1,8 +1,7 @@
 <template>
   <button
     type="button"
-    class="rounded-md px-1.5 py-1 text-sm font-medium shadow-sm bg-white dark:bg-zinc-800 hover:bg-zinc-200
-    dark:hover:bg-zinc-700 focus:(outline-none ring-1 ring-blue-500)"
+    class="rounded-md px-1.5 py-1 mx-0.5 text-sm bg-transparent border border-transparent hover:(border border-1 border-zinc-500)"
   >
     <slot />
   </button>
