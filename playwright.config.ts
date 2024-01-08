f17ad9b642c041bc1718d644d668373162bdbb0b
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2024-01-08 16:47:00 +0700

    test(e2e): allow different envs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,5 @@
 import { defineConfig, devices } from '@playwright/test'
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests/e2e',
   /* Run tests in files in parallel */
@@ -24,7 +15,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5173/',
+    baseURL: process.env.MY_BASE_URL || 'http://localhost:5173/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
