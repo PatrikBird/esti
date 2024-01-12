ad00ebf42a175461f73cd4b5b69cd97bd751289d
PatrikBird <52401692+PatrikBird@users.noreply.github.com>
2024-01-12 14:22:18 +0700

    feat: enable different vote systems

--- src/pages/new.vue
+++ src/pages/new.vue
@@ -1,8 +1,9 @@
 <script setup lang='ts'>
 import { Timestamp, addDoc, collection, doc, setDoc } from 'firebase/firestore'
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/vue'
 import { voteSystems } from '../utils/voteSystemProvider'
 import { db } from '~/modules/firebase'
-import type { VoteSystem } from '~/types'
+import { VoteSystem } from '~/types'
 
 const router = useRouter()
 const mainStore = useMainStore()
@@ -10,14 +11,29 @@ const mainStore = useMainStore()
 const formSending = ref(false)
 const isObserver = ref(false)
 
-const isCoffeeChecked = ref(true)
-const isQuestionMarkChecked = ref(true)
-const availableVotes = ref(voteSystems[0])
+const hasCoffeeVote = ref(true)
+const hasQuestionVote = ref(true)
+const selectedVoteSystem = ref<{ name: VoteSystem, values: string[] }>(voteSystems[0])
 
-const isShirtMode = ref(false)
-// provide('isShirtMode', isShirtMode)
-// const availableVotes: Ref<string[]> = ref([])
+const availableVotes = computed(() => {
+  let votes = [...selectedVoteSystem.value.values]
+
+  if (hasCoffeeVote.value)
+    votes = [...votes, 'coffee']
+  else
+    votes = votes.filter(vote => vote !== 'coffee')
+
+  if (hasQuestionVote.value)
+    votes = [...votes, '?']
+  else
+    votes = votes.filter(vote => vote !== '?')
+
+  return votes
+})
+
 provide('availableVotes', availableVotes)
+
+const isShirtMode = computed(() => selectedVoteSystem.value.name === VoteSystem.TShirtSizes)
 
 const enteredName = ref('')
 const { nameIsValid, enteredNameTooLong } = useNameValidator(enteredName)
@@ -48,12 +64,12 @@ async function onFormSubmit() {
   router.push(`/session/${mainStore.session.id}`)
 }
 
-// function handleUserOptionSelected(option: string) {
-//   isObserver.value = option === 'Observer'
-// }
-// function handleVoteOptionSelected(option: string) {
-//   isShirtMode.value = option === 'T-Shirt'
-// }
+// TODO: sort array before matching
+const matchingVoteSystem = computed(() => {
+  return voteSystems.find((voteSystem) => {
+    return JSON.stringify(voteSystem.values) === JSON.stringify(props.availableVotes)
+  })
+})
 </script>
 
 <template>
@@ -68,17 +84,68 @@ async function onFormSubmit() {
       <UsernameInput v-model="enteredName" />
     </template>
     <template #formToggle>
-      <!-- <GenericToggle
-        :options="['Voter', 'Observer']" @option-selected="handleUserOptionSelected"
-      />
-      <GenericToggle
-        :options="['Numbers', 'T-Shirt']" @option-selected="handleVoteOptionSelected"
-      /> -->
-      <VoteSystemSelectSetup
-        @question-mark-checked="isQuestionMarkChecked"
-        @coffee-checked="isCoffeeChecked"
-        @selected-vote-system="availableVotes.values"
-      />
+      <label
+        class="block w-full py-1 text-left text-sm font-medium"
+      >
+        Vote System
+        <div class="w-full">
+          <Listbox v-model="selectedVoteSystem">
+            <div class="relative mt-1">
+              <ListboxButton
+                class="relative w-full cursor-default rounded bg-white py-2 pl-3
+                pr-10 text-left shadow focus:outline-none cursor-pointer
+                focus-visible:(border-indigo-500 ring-2 ring-white/75 ring-offset-2 ring-offset-orange-300)
+                sm:text-sm"
+              >
+                <span class="block truncate">{{ selectedVoteSystem.name }}</span>
+                <span class="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                  <span i-heroicons:chevron-up-down />
+                </span>
+              </ListboxButton>
+
+              <transition
+                leave-active-class="transition duration-100 ease-in"
+                leave-from-class="opacity-100"
+                leave-to-class="opacity-0"
+              >
+                <ListboxOptions
+                  class="absolute mt-1 max-h-60 w-full overflow-auto rounded bg-white
+                  py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-none
+                  sm:text-sm"
+                >
+                  <ListboxOption
+                    v-for="voteSystem in voteSystems"
+                    v-slot="{ active, selected }"
+                    :key="voteSystem.name"
+                    :value="voteSystem"
+                    as="template"
+                  >
+                    <li
+                      class="relative cursor-pointer select-none py-2 pl-10 pr-4"
+                      :class="[active ? 'bg-amber-100 text-amber-900' : 'text-gray-900']"
+                    >
+                      <span class="block truncate text-left">{{ voteSystem.name }}</span>
+                      <span
+                        v-if="selected"
+                        class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
+                      >
+                        <span i-material-symbols:check-circle-outline />
+                      </span>
+                    </li>
+                  </ListboxOption>
+                </ListboxOptions>
+              </transition>
+            </div>
+          </Listbox>
+        </div>
+      </label>
+      <div class="flex gap4">
+        <input id="questionMarkCheckbox" v-model="hasQuestionVote" type="checkbox">
+        <label for="questionMarkCheckbox">Question Mark</label>
+
+        <input id="coffeeCheckbox" v-model="hasCoffeeVote" type="checkbox">
+        <label for="coffeeCheckbox">Coffee</label>
+      </div>
     </template>
     <template #formButton>
       <div class="flex items-center gap4">
